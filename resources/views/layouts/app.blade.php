f2b51f9e0b0efee546f32efc55902deff95934ac
Patrick <pcallahan3@mail.greenriver.edu>
2019-03-27 15:32:14 -0700

    Added HTML to layout file

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Task List</title>
+        <link rel="stylesheet" href="/css/app.css" >
+
+        <body>
+          <div class="container">
+            @yield('content')
+          </div>
+
+          <footer>
+            <p>Copyright &copy; 2019</p>
+          </footer>
+
+
+    </body>
+</html>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,12 +1,16 @@
+
+@extends('layouts.app')
+
+@section('content')
+
 <h1>Tasks</h1>
 
 @if(count($tasks) > 0)
   @foreach ($tasks as $task)
-  <div class="well">
+  <div class="jumbotron">
     <h3>{{$task->text}}</h3>
     <span class="label label-danger">{{$task->due}}</span>
   </div>
-
-
   @endforeach
 @endif
+@endsection
